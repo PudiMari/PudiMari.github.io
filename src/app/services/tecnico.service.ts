e98e7eedfeb7d2b4c5fb8998ab69394e5c3095e3
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-26 13:27:08 -0400

    Adiciona ambiente para técnicos

---
+++ src/app/services/tecnico.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {Tecnico} from "../models/tecnico";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TecnicoService {
+
+    baseUrl: String = environment.baseUrl;
+
+    constructor(private http: HttpClient) {
+    }
+
+    findAll(): Observable<Tecnico[]> {
+        const url = this.baseUrl + "/tecnicos";
+        return this.http.get<Tecnico[]>(url);
+    }
+}
