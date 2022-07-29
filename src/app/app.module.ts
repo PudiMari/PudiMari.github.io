8aecd73a0b68d1bee3646b371b052de80d2917f3
PudiMari <mariana.dias@estudante.ifmt.edu.br>
2022-07-29 11:21:04 -0400

    Adiciona componente para atualizar técnico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import {HomeComponent} from './views/components/home/home.component';
 import {TecnicoReadComponent} from './views/components/tecnico/tecnico-read/tecnico-read.component';
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {TecnicoCreateComponent} from './views/components/tecnico/tecnico-create/tecnico-create.component';
+import { TecnicoUpdateComponent } from './views/components/tecnico/tecnico-update/tecnico-update.component';
 
 
 @NgModule({
@@ -36,7 +37,8 @@ import {TecnicoCreateComponent} from './views/components/tecnico/tecnico-create/
         NavComponent,
         HomeComponent,
         TecnicoReadComponent,
-        TecnicoCreateComponent
+        TecnicoCreateComponent,
+        TecnicoUpdateComponent
     ],
     imports: [
         BrowserModule,
